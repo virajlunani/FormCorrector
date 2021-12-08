0a1c651f7559d227353ac3093f73d26069d04219
Viraj Lunani <vlunani1@gmail.com>
2021-12-08 17:34:52 -0500

    added squat correction and curl correction

--- corrector.py
+++ corrector.py
@@ -1,47 +1,68 @@
-import math
+import sys
 import cv2
 import mediapipe as mp
 import numpy as np
-from drawing_style import get_back_correct_drawing_style, get_back_incorrect_drawing_style
-from check_form import back_straight
+from drawing_style import *
+from check_form import *
 
-def main():
-    cap = cv2.VideoCapture(0)
+def main(argv):
+    cap = cv2.VideoCapture('squat.mp4')
     mpDraw = mp.solutions.drawing_utils
     mp_holistic = mp.solutions.holistic
+    stage = None
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while True:
             _, img = cap.read()
+            
             frame_dims = img.shape[1::-1]
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             results = holistic.process(imgRGB)
 
             if results.pose_landmarks:
                 body_landmarks = results.pose_landmarks.landmark
-                if back_straight(body_landmarks, frame_dims):
+                if argv[0] == 'curl':
+                    back_correct = back_straight(body_landmarks, frame_dims)
+                    left_arm_correct = left_arm_valid(body_landmarks, frame_dims)
+                    right_arm_correct = right_arm_valid(body_landmarks, frame_dims)
                     mpDraw.draw_landmarks(
                         img, 
                         results.pose_landmarks, 
                         mp_holistic.POSE_CONNECTIONS,
-                        connection_drawing_spec=get_back_correct_drawing_style()
+                        connection_drawing_spec=get_curl_drawing_style(back_correct, left_arm_correct, right_arm_correct)
                     )
-                else:
+                    '''
+                    if back not straight --> display text on screen "STRAIGHTEN BACK"
+                    if elbow too far forward display text on screen "BRING ELBOW BACK"
+                    if elbow too far back display text on screen "BRING ELBOW FORWARD"
+                    '''
+                elif argv[0] == 'squat':
+                    knee_correct = knee_behind_toe(body_landmarks, frame_dims)
+                    knee_angle = get_knee_angle(body_landmarks, frame_dims)
+
+                    
+                    if knee_angle > 160:
+                        stage = "up"
+                    if knee_angle > 100 and stage == "up":
+                        put_text(img, "SQUAT LOWER", (100, 100))
+                    if knee_angle < 100:
+                        stage = "down"
+                        put_text(img, "GOOD JOB!", (100, 100))
+                    if knee_angle > 100 and knee_angle < 120 and stage == "down":
+                        put_text(img, "GOOD JOB!", (100, 100))
+                    if not knee_correct:
+                        put_text(img, "BRING KNEES BACK!", (100, 130))
+                    
+
                     mpDraw.draw_landmarks(
                         img, 
                         results.pose_landmarks, 
                         mp_holistic.POSE_CONNECTIONS,
-                        connection_drawing_spec=get_back_incorrect_drawing_style()
-                    )
-                
-                
+                        connection_drawing_spec=get_squat_drawing_style(knee_correct)
+                    )                    
 
             cv2.imshow("image", img)
             cv2.waitKey(1)
-def test_tan():
-    x1, y1 = 40, 10
-    x2, y2 = 30, 90
-    print(math.degrees(math.atan(float((x2-x1)/(y2-y1)))))
     
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
--- drawing_style.py
+++ drawing_style.py
@@ -4,10 +4,12 @@ from mediapipe.python.solutions import pose
 from mediapipe.python.solutions.drawing_utils import DrawingSpec
 from mediapipe.python.solutions.pose import PoseLandmark
 from mediapipe.python.solutions.pose_connections import POSE_CONNECTIONS
+import cv2
 
 _RED = (48, 48, 255)
 _GREEN = (48, 255, 48)
 _WHITE = (224, 224, 224)
+_YELLOW = (0, 255, 255)
 
 _THICK =  3
 _THIN = 2
@@ -19,31 +21,52 @@ BACK_LANDMARKS = frozenset([
     PoseLandmark.LEFT_HIP
 ])
 
-def get_back_correct_drawing_style() -> Mapping[Tuple[int, int], DrawingSpec]:
+BACK_CONNECTIONS = [(11, 12), (11, 23), (23, 24), (12, 24)]
+LEFT_ARM_CONNECTIONS = [(11, 13), (13, 15)]
+RIGHT_ARM_CONNECTIONS = [(12, 14), (14, 16)]
+RIGHT_LEG_CONNECTION = (26,28)
+
+CORRECT_SPEC = DrawingSpec(_GREEN, thickness=_THICK)
+INCORRECT_SPEC = DrawingSpec(_RED, thickness=_THICK)
+BODY_SPEC = DrawingSpec(_WHITE, thickness=_THIN)
+
+def get_curl_drawing_style(back_correct, left_arm_correct, right_arm_correct) -> Mapping[Tuple[int, int], DrawingSpec]:
     pose_landmark_style = {}
-    back_connections = [(11, 12), (11, 23), (23, 24), (12, 24)]
-    back_spec = DrawingSpec(_GREEN, thickness=_THICK)
-    body_spec = DrawingSpec(_WHITE, thickness=_THIN)
 
     for connection in POSE_CONNECTIONS:
-        if connection in back_connections:
-            pose_landmark_style[connection] = back_spec
+        if connection in BACK_CONNECTIONS:
+            if back_correct:
+                pose_landmark_style[connection] = CORRECT_SPEC
+            else:
+                pose_landmark_style[connection] = INCORRECT_SPEC
+        elif connection in LEFT_ARM_CONNECTIONS:
+            if left_arm_correct:
+                pose_landmark_style[connection] = CORRECT_SPEC
+            else:
+                pose_landmark_style[connection] = INCORRECT_SPEC
+        elif connection in RIGHT_ARM_CONNECTIONS:
+            if right_arm_correct:
+                pose_landmark_style[connection] = CORRECT_SPEC
+            else:
+                pose_landmark_style[connection] = INCORRECT_SPEC
         else:
-            pose_landmark_style[connection] = body_spec
+            pose_landmark_style[connection] = BODY_SPEC
 
     return pose_landmark_style
 
-def get_back_incorrect_drawing_style() -> Mapping[Tuple[int, int], DrawingSpec]:
+def get_squat_drawing_style(knee_correct) -> Mapping[Tuple[int, int], DrawingSpec]:
     pose_landmark_style = {}
-    back_connections = [(11, 12), (11, 23), (23, 24), (12, 24)]
-    back_spec = DrawingSpec(_RED, thickness=_THICK)
-    body_spec = DrawingSpec(_WHITE, thickness=_THIN)
 
     for connection in POSE_CONNECTIONS:
-        if connection in back_connections:
-            pose_landmark_style[connection] = back_spec
+        if connection == RIGHT_LEG_CONNECTION:
+            if knee_correct:
+                pose_landmark_style[connection] = CORRECT_SPEC
+            else:
+                pose_landmark_style[connection] = INCORRECT_SPEC
         else:
-            pose_landmark_style[connection] = body_spec
+            pose_landmark_style[connection] = BODY_SPEC
 
     return pose_landmark_style
 
+def put_text(img, text, pos):
+    cv2.putText(img, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 1, _YELLOW, 2, cv2.LINE_AA)
